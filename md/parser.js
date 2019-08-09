7bd433716dd0cf8cf9fde74c8d907486a6959a49
ryx1024 <ryx1024@gmail.com>
2019-08-09 11:07:24 +0900

    update posts

--- md/parser.js
+++ md/parser.js
@@ -1,22 +1,5 @@
 function getdata(){
-  var params=location.search;
-  if (!params) return false;
-  params=params.substring(1);
-  var pair=params.split("&");
-  var i=tmp="";
-  var param=new Array();
-  for (i=0; i < pair.length; i++) {
-    tmp=pair[i].split("=");
-    key=tmp[0];
-    val=tmp[1];
-    param[key]=val;
-  }
-  var postid="";
-  if (!param["postid"] || param["postid"]==""){
-    return false;
-    }else{
-    postid=unescape(param["postid"]);
-  }
+  var postid = location.pathname
 
   var req = new XMLHttpRequest();
   var url = postid + ".md";
